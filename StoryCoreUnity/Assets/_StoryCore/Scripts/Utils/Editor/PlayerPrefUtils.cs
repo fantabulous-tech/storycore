02e728f0235163c0dec7d72ecff7b659f735cb69
Fantabulous <fan@fantabulous.tech>
2020-06-12 10:57:17 +1200

    Remove unused Utils

--- StoryCoreUnity/Assets/_StoryCore/Scripts/Utils/Editor/PlayerPrefUtils.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEditor;
-using UnityEngine;
-
-public static class PlayerPrefUtils {
-    [MenuItem("Tools/Reset PlayerPrefs")]
-    private static void DeleteAllPlayerPrefs() {
-        PlayerPrefs.DeleteAll();
-    }
-}
