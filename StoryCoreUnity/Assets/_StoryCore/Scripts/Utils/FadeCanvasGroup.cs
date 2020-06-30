155daf9424799605ee88b61e4ec975955c4fcdb1
Fantabulous <fan@fantabulous.tech>
2020-06-30 22:54:32 +1200

    Support manual FadeCanvasGroup options

--- StoryCoreUnity/Assets/_StoryCore/Scripts/Utils/FadeCanvasGroup.cs
+++ StoryCoreUnity/Assets/_StoryCore/Scripts/Utils/FadeCanvasGroup.cs
@@ -1,18 +1,28 @@
+using System;
 using UnityEngine;
 
 namespace StoryCore.Utils {
     public class FadeCanvasGroup : MonoBehaviour {
         [SerializeField] private CanvasGroup m_CanvasGroup;
+        [SerializeField] private bool m_FadeOnEnable;
+        [SerializeField] private float m_FadeDuration = 1;
+        [SerializeField] private AnimationCurve m_FadeCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+        [SerializeField] private FadeDirection m_Direction;
 
+        private enum FadeDirection {
+            In,
+            Out
+        }
+
+        private float m_CurrentFadeDuration;
+        private AnimationCurve m_CurrentFadeCurve;
         private float m_FadeStartTime;
         private float m_FadeStart;
         private bool m_InTransition;
-        private float m_FadeDuration;
         private float m_FadeTarget;
-        private AnimationCurve m_FadeCurve;
         private DelaySequence m_Delay;
 
-        private float FadeProgress => (Time.time - m_FadeStartTime)/m_FadeDuration;
+        private float FadeProgress => (Time.time - m_FadeStartTime)/m_CurrentFadeDuration;
         private bool FadeComplete => FadeProgress >= 1;
 
         private CanvasGroup CanvasGroup => UnityUtils.GetOrSet(ref m_CanvasGroup, GetComponentInChildren<CanvasGroup>);
@@ -26,8 +36,31 @@ namespace StoryCore.Utils {
             Alpha = 0;
         }
 
+        private void OnEnable() {
+            if (!m_FadeOnEnable) {
+                return;
+            }
+
+            switch (m_Direction) {
+                case FadeDirection.In:
+                    Alpha = 0;
+                    In(m_FadeDuration, m_FadeCurve);
+                    break;
+                case FadeDirection.Out:
+                    Alpha = 1;
+                    Out(m_FadeDuration, m_FadeCurve);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
         private void LateUpdate() {
             UpdateFade();
+        }
+
+        private void Reset() {
+            m_CanvasGroup = GetComponent<CanvasGroup>();
         }
 
         private void UpdateFade() {
@@ -35,7 +68,7 @@ namespace StoryCore.Utils {
                 return;
             }
             if (!FadeComplete) {
-                Alpha = Mathf.Lerp(m_FadeStart, m_FadeTarget, m_FadeCurve.Evaluate(m_FadeTarget.Approximately(0) ? 1 - FadeProgress : FadeProgress));
+                Alpha = Mathf.Lerp(m_FadeStart, m_FadeTarget, m_CurrentFadeCurve.Evaluate(m_FadeTarget.Approximately(0) ? 1 - FadeProgress : FadeProgress));
             } else {
                 CompleteFade();
             }
@@ -63,8 +96,8 @@ namespace StoryCore.Utils {
 
         private DelaySequence To(float target, float duration, AnimationCurve curve) {
             m_FadeTarget = target;
-            m_FadeDuration = duration;
-            m_FadeCurve = curve;
+            m_CurrentFadeDuration = duration;
+            m_CurrentFadeCurve = curve;
             m_FadeStartTime = Time.time;
             m_FadeStart = Alpha;
             m_InTransition = true;
