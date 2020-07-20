670a54e7422ec7101d2df2188b82117b0d3f03c1
Fantabulous <fan@fantabulous.tech>
2020-07-20 19:43:16 +1200

    Add command handler for triggering story functions

---
+++ StoryCoreUnity/Assets/_StoryCore/Scripts/Commands/CommandStoryFunctionHandler.cs
@@ -0,0 +1,23 @@
+using StoryCore.GameVariables;
+using StoryCore.Utils;
+using UnityEngine;
+
+namespace StoryCore.Commands {
+    [CreateAssetMenu(menuName = "Commands/Command Story Function")]
+    public class CommandStoryFunctionHandler : CommandHandler {
+        [SerializeField, AutoFillAsset] private StoryTellerLocator m_StoryTellerLocator;
+        [SerializeField] private string m_Function;
+
+        private StoryTeller StoryTeller => m_StoryTellerLocator.Value;
+
+        public override DelaySequence Run(ScriptCommandInfo info) {
+            if (!StoryTeller) {
+                Debug.LogWarning($"Couldn't call story function {m_Function}. No StoryTeller found.");
+            } else {
+                StoryTeller.Story.EvaluateFunction(m_Function);
+            }
+
+            return DelaySequence.Empty;
+        }
+    }
+}
